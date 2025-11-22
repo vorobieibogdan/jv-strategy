eafd4756e0be1c7547831f66d17d58c934bcd93e
Bohdan Vorobiei <vbs@ukr.net>
2025-11-22 17:55:14 +0200

    tasks were performed

--- src/main/java/core/basesyntax/HelloWorld.java
+++ src/main/java/core/basesyntax/HelloWorld.java
@@ -1,8 +1,58 @@
 package core.basesyntax;
 
+interface DiscountService {
+    double getDiscount();
+}
+
+class DefaultDiscountService implements DiscountService {
+    @Override
+    public double getDiscount() {
+        return 0;
+    }
+}
+
+class NewYearDiscountService implements DiscountService {
+    @Override
+    public double getDiscount() {
+        return 20;
+    }
+}
+
+class BirthdayDiscountService implements DiscountService {
+    @Override
+    public double getDiscount() {
+        return 33;
+    }
+}
+
+class BlackFridayDiscountService implements DiscountService {
+    @Override
+    public double getDiscount() {
+        return 45;
+    }
+}
+
+class DiscountStrategy {
+    public DiscountService getDiscountServiceBySpecialEvent(String specialEvent) {
+        if (specialEvent == null) {
+            return new DefaultDiscountService();
+        }
+
+        switch (specialEvent) {
+            case "Birthday":
+                return new BirthdayDiscountService();
+            case "Black Friday":
+                return new BlackFridayDiscountService();
+            case "New Year":
+                return new NewYearDiscountService();
+            default:
+                return new DefaultDiscountService();
+        }
+    }
+}
+
 /**
  * Feel free to remove this class and create your own.
  */
 public class HelloWorld {
-
 }
