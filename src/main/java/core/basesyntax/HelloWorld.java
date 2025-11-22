3f06ff72976fb08623bf69b2421b9f8225a34fa6
Bohdan Vorobiei <vbs@ukr.net>
2025-11-22 17:58:14 +0200

    tasks were performed2

--- src/main/java/core/basesyntax/HelloWorld.java
+++ src/main/java/core/basesyntax/HelloWorld.java
@@ -1,58 +1,55 @@
 package core.basesyntax;
 
-interface DiscountService {
-    double getDiscount();
-}
-
-class DefaultDiscountService implements DiscountService {
-    @Override
-    public double getDiscount() {
-        return 0;
-    }
-}
-
-class NewYearDiscountService implements DiscountService {
-    @Override
-    public double getDiscount() {
-        return 20;
-    }
-}
-
-class BirthdayDiscountService implements DiscountService {
-    @Override
-    public double getDiscount() {
-        return 33;
-    }
-}
-
-class BlackFridayDiscountService implements DiscountService {
-    @Override
-    public double getDiscount() {
-        return 45;
-    }
-}
-
-class DiscountStrategy {
-    public DiscountService getDiscountServiceBySpecialEvent(String specialEvent) {
-        if (specialEvent == null) {
-            return new DefaultDiscountService();
-        }
-
-        switch (specialEvent) {
-            case "Birthday":
-                return new BirthdayDiscountService();
-            case "Black Friday":
-                return new BlackFridayDiscountService();
-            case "New Year":
-                return new NewYearDiscountService();
-            default:
-                return new DefaultDiscountService();
-        }
-    }
-}
-
-/**
- * Feel free to remove this class and create your own.
- */
 public class HelloWorld {
+
+    public interface DiscountService {
+        double getDiscount();
+    }
+
+    public static class DefaultDiscountService implements DiscountService {
+        @Override
+        public double getDiscount() {
+            return 0;
+        }
+    }
+
+    public static class NewYearDiscountService implements DiscountService {
+        @Override
+        public double getDiscount() {
+            return 20;
+        }
+    }
+
+    public static class BirthdayDiscountService implements DiscountService {
+        @Override
+        public double getDiscount() {
+            return 33;
+        }
+    }
+
+    public static class BlackFridayDiscountService implements DiscountService {
+        @Override
+        public double getDiscount() {
+            return 45;
+        }
+    }
+
+    public static class DiscountStrategy {
+        public DiscountService getDiscountServiceBySpecialEvent(String specialEvent) {
+            if (specialEvent == null) {
+                return new DefaultDiscountService();
+            }
+            switch (specialEvent) {
+                case "Birthday":
+                    return new BirthdayDiscountService();
+                case "Black Friday":
+                    return new BlackFridayDiscountService();
+                case "New Year":
+                    return new NewYearDiscountService();
+                default:
+                    return new DefaultDiscountService();
+            }
+        }
+    }
 }
+
